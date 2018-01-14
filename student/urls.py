3d1c4ca8d9831a240c5419147d132e2eff3ade3d
wwiras <wwiras@gmail.com>
2018-01-15 05:43:11 +0800

    Student detail

--- student/urls.py
+++ student/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$',views.home,name='student_home'),
     url(r'^home/$',views.home_json,name='student_home_json'),
     url(r'^new/$', views.student_new, name='student_new'),
+    url(r'^(?P<pk>\d+)/$', views.student_detail, name='student_detail'),
     url(r'^list_json/$', views.student_list_json.as_view(), name="student_list_json"),
 ]
