fa4a3c3d387f622d60220167a6a472bd2857ae6f
wwiras <wwiras@gmail.com>
2018-01-15 13:57:32 +0800

    Add Authentication

--- student/urls.py
+++ student/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include,url
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     # url(r'^$',views.home,name='student_home'),
@@ -8,6 +9,7 @@ urlpatterns = [
     url(r'^$',views.home_json,name='student_home'),
     url(r'^new/$', views.student_new, name='student_new'),
     url(r'^(?P<pk>\d+)/$', views.student_detail, name='student_detail'),
-    url(r'^list_json/$', views.student_list_json.as_view(), name="student_list_json"),
+    # url(r'^list_json/$', views.student_list_json.as_view(), name="student_list_json"),
     url(r'^(?P<pk>\d+)/edit$', views.student_edit, name='student_edit'),
+    url(r'^list_json/$', login_required(views.student_list_json.as_view()), name="student_list_json"),
 ]
--- student/views.py
+++ student/views.py
@@ -11,12 +11,15 @@ from django.db.models import Count, Sum, Q, Case, Value, When, IntegerField
 
 # Create your views here.
 
+@login_required(login_url='/accounts/login/')
 def home(request):
 	return render(request,'base.html')
 
+@login_required(login_url='/accounts/login/')
 def home_json(request):
     return render(request, 'student/home_json.html')
 
+@login_required(login_url='/accounts/login/')
 def student_new(request):
 
     if request.method == "POST":
@@ -32,6 +35,7 @@ def student_new(request):
     print(request.user)
     return render(request, 'student/student_new.html', {'form': form})
 
+@login_required(login_url='/accounts/login/')
 def student_edit(request,pk):
 
     student = get_object_or_404(Student, pk=pk)
@@ -49,10 +53,12 @@ def student_edit(request,pk):
     
     return render(request, 'student/student_edit.html', {'form': form})
 
+@login_required(login_url='/accounts/login/')
 def student_detail(request,pk):
     student = get_object_or_404(Student, pk=pk)
     return render(request, 'student/student_detail.html', {'student': student})
 
+@login_required(login_url='/accounts/login/')
 def student_remove(request,pk):
 
     student = get_object_or_404(Student, pk=pk)
